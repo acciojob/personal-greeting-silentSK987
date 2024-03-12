9d582f6b2667d8494f5e57f1fd227beca38f25e4
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-12 12:41:42 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -17,8 +17,8 @@ class Container extends Component {
   render() {
     return (
       <>
-      <label htmlFor="input">Enter your name: </label>
-        <input id="input" value={this.state.value} onChange={this.onChangeText} />
+      <p>Enter your name: </p>
+        <input value={this.state.value} onChange={this.onChangeText} />
         {this.state.value?<p>Hello {this.state.value}! </p> : null} 
       </>
     )
