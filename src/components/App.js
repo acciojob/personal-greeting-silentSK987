55685b3482af9835979a177a66d0b92e45a580b2
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-12 12:39:04 +0000

    update greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Component } from "react";
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
+  onChangeText = (event) => {
+    const updateValue = event.target.value;
+    this.setState({ value: updateValue });
+
+  }
+
+  render() {
+    return (
+      <>
+      <label htmlFor="input">Enter your name: </label>
+        <input id="input" value={this.state.value} onChange={this.onChangeText} />
+        {this.state.value?<p>Hello {this.state.value}! </p> : null} 
+      </>
+    )
+  }
+}
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Container />
+
     </div>
   )
 }
