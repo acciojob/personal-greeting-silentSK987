5ead0c6f71d354682575b9bc34f50e8539534499
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-15 06:37:48 +0000

    done functional comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
 
 import React from "react";
 import './../styles/App.css';
-import { Component } from "react";
+import { useState } from "react";
 
-class Container extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-  }
-  onChangeText = (event) => {
-    const updateValue = event.target.value;
-    this.setState({ value: updateValue });
 
-  }
-
-  render() {
-    return (
-      <>
-      <p>Enter your name: </p>
-        <input value={this.state.value} onChange={this.onChangeText} />
-      {this.state.value?<p>Hello {this.state.value}! </p> : null} 
+function Container(){
+  const [userName,setUserName] = useState("");
+  
+  return (
+    <div>
+    {
+      /* Do not remove the main div */
+      /* empty fragments  */
+      <> 
+      <form action="">
+        <input type="text" onChange={(e)=>setUserName(e.target.value)}/>
+      </form>
+      <p>Hello {userName}!</p>
       </>
-    )
-  }
+    }
+</div>
+
+  )
 }
+
 
 const App = () => {
   return (
@@ -36,3 +36,25 @@ const App = () => {
 }
 
 export default App
+//------ approach with class Component---------------
+// class Container extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: "" };
+//   }
+//   onChangeText = (event) => {
+//     const updateValue = event.target.value;
+//     this.setState({ value: updateValue });
+
+//   }
+
+//   render() {
+//     return (
+//       <>
+//       <p>Enter your name: </p>
+//         <input value={this.state.value} onChange={this.onChangeText} />
+//       {this.state.value?<p>Hello {this.state.value}! </p> : null} 
+//       </>
+//     )
+//   }
+// }
